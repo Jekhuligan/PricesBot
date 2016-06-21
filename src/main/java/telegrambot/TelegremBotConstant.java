e57d28789adcff6c9dc5b80a06ba3fb4ef86a10a
Jekhuligan <Jekhuligan@gmail.com>
2016-06-22 01:09:38 +0400

    Add bot to repository

--- src/main/java/telegrambot/TelegremBotConstant.java
+++ src/main/java/telegrambot/TelegremBotConstant.java
@@ -5,6 +5,9 @@ package telegrambot;
  */
 public interface  TelegremBotConstant
 {
-    public static final String TOKEN_TELEGRAM_BOT = "182525384:AAGvF0xbZyzH3motrOK_nbqKYMZMS9J-hw4";
-    public static final String USERNAME_TELEGRAM_BOT = "QuestionTelegramBot";
+    /* Old bot for telegram.me/QuestionTelegramBot */
+    public static final String TOKEN_TELEGRAM_BOT_OLD = "182525384:AAGvF0xbZyzH3motrOK_nbqKYMZMS9J-hw4";
+    /* @Prices_bot or telegram.me/Prices_bot*/
+    public static final String TOKEN_TELEGRAM_BOT = "238455965:AAE5ujJxSx7T_IPy2gIwrQZjHVLB4ilKKcc";
+    public static final String USERNAME_TELEGRAM_BOT = "Prices_bot";
 }
